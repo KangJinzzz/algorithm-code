293b6ee55b6d1e2f34ff5dc1bd9096b3e730f885
kangJinzzz <1243363939@qq.com>
2019-09-13 15:18:58 +0800

    提交

--- leetcode/leetcode/源.c
+++ leetcode/leetcode/源.c
@@ -226,7 +226,7 @@ int main()
 //
 //你不需要考虑数组中超出新长度后面的元素。
 
-#if 1
+#if 0
 
 int removeDuplicates(int* nums, int numsSize){
 	//if (numsSize == 0)
@@ -273,6 +273,57 @@ int main()
 	int arr[] = { 0,0,1,1, 1, 2 , 2, 3 ,3 , 4};
 	int sz = sizeof(arr) / sizeof(arr[0]);
 	int ret = removeDuplicates(arr, sz);
+	printf("%d\n", ret);
+	int i = 0;
+	for (i = 0; i < ret; i++)
+	{
+		printf("%d", arr[i]);
+	}
+	printf("\n");
+	return 0;
+}
+#endif
+
+//27.给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
+//
+//不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+//
+//元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+//
+//示例 1:
+//
+//给定 nums = [3, 2, 2, 3], val = 3,
+//
+//函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。
+//
+//你不需要考虑数组中超出新长度后面的元素。
+
+
+#if 1
+
+int removeElement(int* nums, int numsSize, int val){
+	if (numsSize == 0)
+	{
+		return 0;
+	}
+	int i = 0;
+	int k = 0;
+	for (i = 0; i < numsSize; i++)
+	{
+		if (nums[i] != val)
+		{
+			nums[k++] = nums[i];
+		}
+	}
+	return k;
+}
+
+
+int main()
+{
+	int arr[] = { 3, 2, 2, 3 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int ret = removeElement(arr, sz, 3);
 	printf("%d\n", ret);
 	int i = 0;
 	for (i = 0; i < ret; i++)
