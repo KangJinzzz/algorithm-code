a61e957460afd9e63e2c9e6b9d1a529aaa5b62b2
kangJinzzz <1243363939@qq.com>
2019-09-14 20:57:48 +0800

    提交

--- leetcode/leetcode/源.c
+++ leetcode/leetcode/源.c
@@ -344,7 +344,7 @@ int main()
 //输入 : [1, 3, 5, 6], 5
 //输出 : 2
 
-#if 1
+#if 0
 int searchInsert(int* nums, int numsSize, int target){
 	//if (numsSize == 0)
 	//{
@@ -375,6 +375,25 @@ int main()
 	printf("%d\n", ret);
 	return 0;
 
+}
+#endif
+
+//给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+//
+//示例 :
+//
+//输入 : [-2, 1, -3, 4, -1, 2, 1, -5, 4],
+//输出 : 6
+// 解释 : 连续子数组 [4, -1, 2, 1] 的和最大，为 6。
+
+
+#if 1
+
+
+int main()
+{
+
+	return 0;
 }
 #endif
 
