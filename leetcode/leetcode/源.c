55db54dbf902de02cfd6b14599e0f115f0a3302b
kangJinzzz <1243363939@qq.com>
2019-09-16 07:33:57 +0800

    提交

--- leetcode/leetcode/源.c
+++ leetcode/leetcode/源.c
@@ -385,6 +385,7 @@ int main()
 //输入 : [-2, 1, -3, 4, -1, 2, 1, -5, 4],
 //输出 : 6
 // 解释 : 连续子数组 [4, -1, 2, 1] 的和最大，为 6。
+#if 0
 
 int maxSubArray(int* nums, int numsSize){
 	//if (numsSize == 0)
@@ -413,9 +414,6 @@ int maxSubArray(int* nums, int numsSize){
 	
 }
 
-#if 1
-
-
 int main()
 {
 	int arr[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
@@ -427,6 +425,44 @@ int main()
 }
 #endif
 
+//给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+//
+//最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+//
+//你可以假设除了整数 0 之外，这个整数不会以零开头。
+//
+//示例 1 :
+//
+//输入 : [1, 2, 3]
+//输出 : [1, 2, 4]
+// 解释 : 输入数组表示数字 123。
+
+#if 1
+int* plusOne(int* digits, int digitsSize, int* returnSize){
+	int *ret = (int *)malloc(sizeof(int)*digitsSize);
+	for (int i = 0; i < digitsSize; i++)
+	{
+		ret[i] = digits[i];
+
+		if (i = digitsSize)
+		{
+			ret[i] = digits[i] + 1;
+		}
+	}
+	return ret;
+}
+int main()
+{
+	int arr[] = { 1, 2, 3 };
+	int b[3] = { 0 };
+	plusOne(arr, 3, b);
+	for (int i = 0; i < 3; i++)
+	{
+		printf("%d ", b[i]);
+	}
+	return 0;
+}
+#endif
 
 
 
