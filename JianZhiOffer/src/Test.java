7b16e3e1fe741b9425d5d89ab506a809f739b27f
kangJinzzz <1243363939@qq.com>
2019-10-19 09:39:03 +0800

    提交

--- JianZhiOffer/src/Test.java
+++ JianZhiOffer/src/Test.java
@@ -118,6 +118,23 @@ public class Test {
         }
         return c;
     }
-
+    //青蛙跳台阶问题
+    public int JumpFloor(int target) {
+        int a = 1;
+        int b = 2;
+        int c = 0;
+        if(target == 1) {
+            return 1;
+        }
+        if(target == 2) {
+            return 2;
+        }
+        for(int i = 2; i < target; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
 }
 
