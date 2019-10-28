94df2a1595730be28da9e600e33cdaebe805c049
kangJinzzz <1243363939@qq.com>
2019-10-28 10:05:36 +0800

    提交

--- JianZhiOffer/src/Test.java
+++ JianZhiOffer/src/Test.java
@@ -1,11 +1,12 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
         int[] numbers = {0,1,2,3,4,5,6};
-        int length = numbers.length;
-        int[] duplication = new int[1];
-        System.out.println(duplicate(numbers, length, duplication));
+        reOrderArray(numbers);
+
+        System.out.println(Arrays.toString(numbers));
 
     }
 
@@ -261,5 +262,38 @@ public class Test {
         }
         return newHead.next;
     }
+
+    //面试题21：调整数组顺序使奇数位于偶数前面
+    public static void reOrderArray(int [] array) {
+
+        int len = array.length;
+        if(len <= 1){ // 数组空或长度为1
+            return;
+        }
+
+        int i = 0;
+        while(i < len){
+            int j = i + 1;
+            if(array[i]%2 == 0){ // a[i]为偶数，j前进，直到替换
+                while(array[j]%2 == 0){ // j为偶数，前进
+                    if(j==len-1)// i为偶数，j也为偶数，一直后移到了末尾，证明后面都是偶数
+                        return;
+                    j++;
+                }
+                // 此时j为奇数
+                int count = j-i;
+                int temp = array[i];
+                array[i] = array[j];
+                while(count>1){
+                    array[i+count] = array[i+count-1];//数组后移
+                    count--;
+                }
+                array[i+1] = temp;
+            }
+            i++;
+        }
+
+
+   }
 }
 
