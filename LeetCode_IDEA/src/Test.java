3933e455729f189b3765af70333b3a3762a8224f
kangJinzzz <1243363939@qq.com>
2019-11-07 09:28:41 +0800

    提交

--- LeetCode_IDEA/src/Test.java
+++ LeetCode_IDEA/src/Test.java
@@ -543,6 +543,21 @@ public class Test {
         return list;
     }
 
+    //二叉树后序遍历
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root == null) {
+            return list;
+        }
+
+        list.addAll(postorderTraversal(root.left));
+
+        list.addAll(postorderTraversal(root.right));
+        list.add(root.val);
+
+        return list;
+    }
+
 
 }
 
