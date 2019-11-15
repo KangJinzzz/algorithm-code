431a54cedd44c8fcf46fc08fe852aad4ec723c79
kangJinzzz <1243363939@qq.com>
2019-11-15 18:02:55 +0800

    提交

--- NiuKe/src/Solution.java
+++ NiuKe/src/Solution.java
@@ -1,6 +1,30 @@
 public class Solution {
 
+    //输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。要求不能创建任何新的结点，只能调整树中结点指针的指向
+    public TreeNode Convert(TreeNode pRootOfTree) {
+        if (pRootOfTree == null) {
+            return null;
+        }
+        if(pRootOfTree.left == null && pRootOfTree.right== null) {
+            return pRootOfTree;
+        }
+        TreeNode left = Convert(pRootOfTree.left);
+        TreeNode leftTail = left;
+        while(leftTail != null && leftTail.right != null) {
+            leftTail = leftTail.right;
+        }
+        if(left != null) {
+            leftTail.right = pRootOfTree;
+            pRootOfTree.left = leftTail;
+        }
+        TreeNode right = Convert(pRootOfTree.right);
+        if(right != null) {
+            pRootOfTree.right = right;
+            right.left = pRootOfTree;
+        }
+        return left != null ? left : pRootOfTree;
 
+    }
 
 
 
