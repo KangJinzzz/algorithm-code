68fae108923156abfda3ab8a79976bfba3b08757
kangJinzzz <1243363939@qq.com>
2019-11-24 09:04:32 +0800

    提交

--- NiuKe/src/Solution.java
+++ NiuKe/src/Solution.java
@@ -1,3 +1,7 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.*;
 public class Solution {
 
     //输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。要求不能创建任何新的结点，只能调整树中结点指针的指向
@@ -28,4 +32,34 @@ public class Solution {
 
 
 
+}
+
+//[编程题]旧键盘 (20)
+class Main0 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Set<Character> actual = new HashSet<>();
+        Set<Character> print = new HashSet<>();
+        while (scanner.hasNext()) {
+            String s1 = scanner.next();
+            String s2 = scanner.next();
+            s1 = s1.toUpperCase();
+            s2 = s2.toUpperCase();
+            for(int i = 0; i < s2.length(); i++) {
+                actual.add(s2.charAt(i));
+            }
+            for(int i = 0; i < s1.length(); i++) {
+                char c = s1.charAt(i);
+                //
+                if(actual.contains(c)) {
+                    continue;
+                }
+                if(print.contains(c)) {
+                    continue;
+                }
+                System.out.print(c);
+                print.add(c);
+            }
+        }
+    }
 }
