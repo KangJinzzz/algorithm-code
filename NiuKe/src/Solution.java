c6e0492ab82c844c96371b7351eb556c4f03a65b
kangJinzzz <1243363939@qq.com>
2019-12-07 21:24:47 +0800

    提交

--- NiuKe/src/Solution.java
+++ NiuKe/src/Solution.java
@@ -1,7 +1,6 @@
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
-import java.util.*;
 public class Solution {
 
     //输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。要求不能创建任何新的结点，只能调整树中结点指针的指向
@@ -60,6 +59,62 @@ class Main0 {
                 System.out.print(c);
                 print.add(c);
             }
+        }
+    }
+}
+
+
+//下厨房  错误！未通过
+class Main1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNext()) {
+            Set<String> set = new HashSet<>();
+            String material = scanner.next();
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < material.length(); i++) {
+                if(material.charAt(i) != ' ') {
+                    stringBuilder.append(material.charAt(i));
+                }
+                else {
+                    set.add(stringBuilder.toString());
+                    stringBuilder.delete(0, stringBuilder.length());
+                }
+            }
+            System.out.println(set.size());
+        }
+
+    }
+}
+
+//Fibonacci数列
+class Main2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int n = sc.nextInt();
+            int a = 0;
+            int b = 1;
+            int c = a + b;
+            while (true) {
+                if(n == c) {
+                    System.out.println(0);
+                    return;
+                } else if(n > c) {
+                    a = b;
+                    b = c;
+                    c = a + b;
+                } else {
+                    int min = n - b;
+                    if((c - n) < min) {
+                        min = c - n;
+                    }
+                    System.out.println(min);
+                    return;
+                }
+            }
+
         }
     }
 }
