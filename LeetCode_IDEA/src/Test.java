adcac6be69bec8b690728a1083ae4cdb633fd52f
kangJinzzz <1243363939@qq.com>
2020-01-14 15:28:33 +0800

    提交

--- LeetCode_IDEA/src/Test.java
+++ LeetCode_IDEA/src/Test.java
@@ -1038,8 +1038,17 @@ public class Test {
 //    给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它
 //    将会被按顺序插入的位置,你可以假设数组中无重复元素
     public int searchInsert(int[] nums, int target) {
-
+        for (int i = 0; i < nums.length; i++) {
+            if(target == nums[i]) {
+                return i;
+            }
+            if(target < nums[i]) {
+                return i;
+            }
+        }
+        return nums.length;
     }
+
 
 
 
