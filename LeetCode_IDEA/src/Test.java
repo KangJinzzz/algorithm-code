e683b454696f6377af32b0dca3ddc55a36bb7b39
kangJinzzz <1243363939@qq.com>
2020-01-17 13:35:25 +0800

    提交

--- LeetCode_IDEA/src/Test.java
+++ LeetCode_IDEA/src/Test.java
@@ -1079,6 +1079,29 @@ public class Test {
         return x == n || x == (n / 10);
     }
 
+//    58. 最后一个单词的长度
+//    给定一个仅包含大小写字母和空格 ' ' 的字符串 s，返回其最后一个单词的长度。
+//    如果字符串从左向右滚动显示，那么最后一个单词就是最后出现的单词。
+//    如果不存在最后一个单词，请返回 0 。
+    public int lengthOfLastWord(String s) {
+        int index = -1;
+        int count = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                index = i;
+                break;
+            }
+        }
+        if (index == -1) {
+            return 0;
+        } else {
+            while (index >= 0 && s.charAt(index--) != ' ') {
+                count++;
+            }
+        }
+        return count;
+    }
+
 
 
 
