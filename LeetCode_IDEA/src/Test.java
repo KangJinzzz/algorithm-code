77c0b12ae12c78760b1928df938728aaeec81ab5
kangJinzzz <1243363939@qq.com>
2020-01-17 13:17:55 +0800

    提交

--- LeetCode_IDEA/src/Test.java
+++ LeetCode_IDEA/src/Test.java
@@ -1064,6 +1064,21 @@ public class Test {
         return true;
     }
 
+//    9. 回文数
+//    判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 ==0 && x != 0)) {
+            return false;
+        }
+        int n = 0;
+        while (x > n) {
+            int y = x % 10;
+            n = n * 10 + y;
+            x /= 10;
+        }
+        return x == n || x == (n / 10);
+    }
+
 
 
 
