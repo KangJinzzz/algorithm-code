b5ecdf1a2bc36df9e1585b049edd172e13d4a7ff
kangJinzzz <1243363939@qq.com>
2020-02-02 09:28:59 +0800

    提交

--- LeetCode_IDEA/src/Test.java
+++ LeetCode_IDEA/src/Test.java
@@ -1245,6 +1245,18 @@ public class Test {
         return set.size() > 2 ? set.first() : set.last();
     }
 
+//    1. 两数之和
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int n = target - nums[i];
+            if (map.containsKey(n)) {
+                return new int[] {map.get(n), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
 
 }
 
