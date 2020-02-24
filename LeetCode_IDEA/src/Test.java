a3d8347c7a9892e697aaeea604c6229b227b8d28
kangJinzzz <1243363939@qq.com>
2020-02-24 18:34:48 +0800

    提交

--- LeetCode_IDEA/src/Test.java
+++ LeetCode_IDEA/src/Test.java
@@ -1505,6 +1505,19 @@ public class Test {
 //
 //    }
 
+//    1281. 整数的各位积和之差
+    public int subtractProductAndSum(int n) {
+        int mul = 1;
+        int sum = 0;
+        while (n > 0) {
+            mul *= n % 10;
+            sum += n % 10;
+            n /= 10;
+        }
+        return mul - sum;
+    }
+
+
 
 
 }
