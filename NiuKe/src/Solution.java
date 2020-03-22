562c157b7c22e294a02634cfaca5210168071078
kangJinzzz <1243363939@qq.com>
2020-03-22 08:31:44 +0800

    提交

--- NiuKe/src/Solution.java
+++ NiuKe/src/Solution.java
@@ -993,3 +993,16 @@ class Main26 {
         }
     }
 }
+
+//百万富翁问题
+class Main27 {
+    public static void main(String[] args){
+        long sumRich=0;
+        long sumStranger=0;
+        for(int i=1;i<=30;i++){
+            sumRich+=10;
+            sumStranger=sumStranger+(long)Math.pow(2,i-1);
+        }
+        System.out.print(sumRich+" "+sumStranger);
+    }
+}
