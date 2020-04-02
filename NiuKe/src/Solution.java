4445324aee8db2bfb840391290c9c7d1976b171e
kangJinzzz <1243363939@qq.com>
2020-04-02 08:48:35 +0800

    提交

--- NiuKe/src/Solution.java
+++ NiuKe/src/Solution.java
@@ -1539,3 +1539,29 @@ class Main39 {
         return ret;
     }
 }
+
+//守形数
+class Main40 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int n = sc.nextInt();
+            boolean flag = true;
+            int n2 = n * n;
+            while (n > 0) {
+                if ((n % 10) != (n2 % 10)) {
+                    flag = false;
+                    break;
+                }
+                n /= 10;
+                n2 /= 10;
+            }
+            if (flag) {
+                System.out.println("Yes!");
+            } else {
+                System.out.println("No!");
+            }
+        }
+    }
+}
+
