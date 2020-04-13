f73b5ef928ce67b4364a5c1972dabf0defdb2936
kangJinzzz <1243363939@qq.com>
2020-04-13 08:49:40 +0800

    提交

--- LeetCode_IDEA/src/Test.java
+++ LeetCode_IDEA/src/Test.java
@@ -1593,7 +1593,39 @@ public class Test {
         return ret;
     }
 
+//买卖股票的最佳时机
+public int maxProfit(int[] prices) {
+    int min = Integer.MAX_VALUE;
+    int max = 0;
+    for (int i = 0; i < prices.length; i++) {
+        if (prices[i] < min) {
+            min = prices[i];
+        } else if (prices[i] - min > max){
+            max = prices[i] - min;
+        }
+    }
+    return max;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
 
 
 
