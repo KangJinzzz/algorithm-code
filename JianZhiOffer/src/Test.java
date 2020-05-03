c446d6f6faab6db956a2af9f460a274f765794b8
kangJinzzz <1243363939@qq.com>
2020-05-03 17:39:05 +0800

    提交

--- JianZhiOffer/src/Test.java
+++ JianZhiOffer/src/Test.java
@@ -548,3 +548,26 @@ class MinStack {
         return B.peek();
     }
 }
+
+//面试题31. 栈的压入、弹出序列
+//    算法流程：
+//        初始化： 辅助栈 stackstack ，弹出序列的索引 ii ；
+//        遍历压栈序列： 各元素记为 numnum ；
+//        元素 numnum 入栈；
+//        循环出栈：若 stackstack 的栈顶元素 == 弹出序列元素 popped[i]popped[i] ，则执行出栈与 i++i++ ；
+//        返回值： 若 stackstack 为空，则此弹出序列合法。
+
+class Solution31 {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stack = new Stack<>();
+        int i = 0;
+        for (int x : pushed) {
+            stack.push(x);
+            while (!stack.isEmpty() && stack.peek() == popped[i]) {
+                stack.pop();
+                i++;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
