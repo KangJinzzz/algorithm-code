97c10c703ba783a968fcfbd559466c77423ec1e9
kangJinzzz <1243363939@qq.com>
2020-05-06 08:38:42 +0800

    提交

--- JianZhiOffer/src/Test.java
+++ JianZhiOffer/src/Test.java
@@ -617,3 +617,33 @@ class Solution32_2 {
         return list;
     }
 }
+
+//面试题32 - III. 从上到下打印二叉树 III
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root == null) {
+            return res;
+        }
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            LinkedList<Integer> tmp = new LinkedList<>();
+            for (int i = queue.size(); i > 0; i--) {
+                TreeNode node = queue.poll();
+                if (res.size() % 2 == 0) tmp.addLast(node.val);
+                else tmp.addFirst(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            res.add(tmp);
+        }
+        return res;
+    }
+}
+
+
