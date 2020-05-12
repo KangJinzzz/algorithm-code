4b66b072d2a817e843494608e9ea82da30dc2931
kangJinzzz <1243363939@qq.com>
2020-05-12 08:27:03 +0800

    提交

--- NiuKe/src/Solution.java
+++ NiuKe/src/Solution.java
@@ -2963,4 +2963,40 @@ class Main73 {
     }
 }
 
-
+//edit-distance
+/*
+    F(i,j):word1的前i个字符于word2的前j个字符的编辑距离
+    状态递推：
+    F(i,j) = min { F(i-1,j）+1, F(i,j-1) +1, F(i-1,j-1) +(w1[i]==w2[j]?0:1) }
+    上式表示从删除，增加和替换操作中选择一个最小操作数
+    F(i-1,j): w1[1,...,i-1]于w2[1,...,j]的编辑距离，删除w1[i]的字符--->F(i,j)
+    F(i,j-1): w1[1,...,i]于w2[1,...,j-1]的编辑距离，增加一个字符--->F(i,j)
+    F(i-1,j-1): w1[1,...,i-1]于w2[1,...,j-1]的编辑距离，如果w1[i]与w2[j]相同，
+    不做任何操作，编辑距离不变，如果w1[i]与w2[j]不同，替换w1[i]的字符为w2[j]--->F(i,j)
+    初始化：
+    初始化一定要是确定的值，如果这里不加入空串，初始值无法确定
+    F(i,0) = i :word与空串的编辑距离，删除操作
+    F(0,i) = i :空串与word的编辑距离，增加操作
+    返回结果：F(m,n)
+*/
+class Solution8 {
+    public int minDistance(String word1, String word2) {
+        int row = word1.length();
+        int col = word2.length();
+        int[][] minDis = new int[row + 1][col + 1];
+        //初始化
+        for (int i = 0; i <= row; i++) {
+            minDis[i][0] = i;
+        }
+        for (int i = 0; i <= col; i++) {
+            minDis[0][i] = i;
+        }
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+                minDis[i][j] = Math.min(minDis[i - 1][j] + 1, minDis[i][j - 1] + 1);
+                minDis[i][j] = Math.min(minDis[i][j], minDis[i - 1][j - 1] + (word1.charAt(i - 1)== word2.charAt(j - 1) ? 0 : 1 ));
+            }
+        }
+        return minDis[row][col];
+    }
+}
