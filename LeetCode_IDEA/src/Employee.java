3136de636a887c557594e4c7b98e9406e580f143
kangJinzzz <1243363939@qq.com>
2020-05-15 09:36:00 +0800

    提交

---
+++ LeetCode_IDEA/src/Employee.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+class Employee {
+    public int id;
+    public int importance;
+    public List<Integer> subordinates;
+};
---
+++ LeetCode_IDEA/src/Test_01.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class Test_01 {
+
+    public static void main(String[] args) {
+        int[] ar = new int[] {2, 6, 4, 8, 10, 9, 15};
+
+
+
+    }
+
+
+
+}
