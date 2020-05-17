1d44fdd93c5d5702044f8e9aee901bab403e0bb0
kangJinzzz <1243363939@qq.com>
2020-05-17 10:09:22 +0800

    提交

--- LeetCode_IDEA/src/Test.java
+++ LeetCode_IDEA/src/Test.java
@@ -2067,3 +2067,28 @@ class Solution4 {
 
     }
 }
+
+//429. N叉树的层序遍历
+class Solution5 {
+    public List<List<Integer>> levelOrder(Node root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> list = new ArrayList<>();
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> tmp = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node node = queue.poll();
+                tmp.add(node.val);
+                for (Node n : node.children) {
+                    queue.offer(n);
+                }
+            }
+            list.add(tmp);
+        }
+        return list;
+    }
+}
