c441b73fa7401781a1666125b1f2ecb09f819c61
kangJinzzz <1243363939@qq.com>
2020-07-07 10:12:19 +0800

    提交

--- JianZhiOffer/src/Test.java
+++ JianZhiOffer/src/Test.java
@@ -1842,3 +1842,28 @@ class Solution68_1 {
         return lowestCommonAncestor(root.right, p, q);
     }
 }
+
+
+//  剑指 Offer 68 - II. 二叉树的最近公共祖先
+class Solution68_2 {
+    TreeNode loc;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        findNode(root, p, q);
+        return loc;
+    }
+    public boolean findNode(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return false;
+        }
+        int left = findNode(root.left, p, q) ? 1 : 0;
+        int right = findNode(root.right, p, q) ? 1 : 0;
+        int mid = (root == p || root == q) ? 1 : 0;
+        if (left + right + mid >= 2) {
+            loc = root;
+        }
+        return left + right + mid >= 1;
+    }
+}
