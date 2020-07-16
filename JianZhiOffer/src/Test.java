8906785c317f09ba014d717088d1039b83e29163
kangJinzzz <1243363939@qq.com>
2020-07-16 08:02:53 +0800

    提交

--- JianZhiOffer/src/Test.java
+++ JianZhiOffer/src/Test.java
@@ -2189,3 +2189,24 @@ class CQueue109 {
         return stack2.pop();
     }
 }
+
+//    剑指 Offer 10- I. 斐波那契数列
+class Solution110 {
+    public int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        for (int i = 3; i <= n; i++) {
+            c = (a + b) % 1000000007;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+}
