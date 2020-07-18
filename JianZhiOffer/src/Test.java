ffe9dcef7b23bd9b7d874ab044ee8e418ce77eda
kangJinzzz <1243363939@qq.com>
2020-07-18 09:09:33 +0800

    提交

--- JianZhiOffer/src/Test.java
+++ JianZhiOffer/src/Test.java
@@ -2312,3 +2312,33 @@ class Solution113 {
         }
     }
 }
+
+//    剑指 Offer 14- I. 剪绳子
+class Solution114 {
+    //动态规划：f（n）:长度为 n 的绳子的最大长度
+    //f(i) = max(f(i) * f(n - i))
+    public int cuttingRope(int n) {
+        if (n < 2) {
+            return 0;
+        }
+        if (n == 2) {
+            return 1;
+        }
+        if (n == 3) {
+            return 2;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        for (int i = 4; i <= n; i++) {
+            for (int j = 1; j <= i / 2; j++) {
+                if (dp[j] * dp[i - j] > dp[i]) {
+                    dp[i] = dp[j] * dp[i - j];
+                }
+            }
+        }
+        return dp[n];
+    }
+}
