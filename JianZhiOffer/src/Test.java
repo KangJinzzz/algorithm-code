ed9ecdf813bcdfee9cad1de995496ee7c1f200db
kangJinzzz <1243363939@qq.com>
2020-07-19 08:59:33 +0800

    提交

--- JianZhiOffer/src/Test.java
+++ JianZhiOffer/src/Test.java
@@ -2359,3 +2359,24 @@ class Solution115 {
         return res;
     }
 }
+
+//剑指 Offer 16. 数值的整数次方
+
+class Solution116 {
+    public double myPow(double x, int n) {
+        double res = 1;
+        long b = n;
+        if (b < 0) {
+            x = 1 / x;
+            b = -b;
+        }
+        while (b > 0) {
+            if ((b & 1) == 1) {
+                res *= x;
+            }
+            x *= x;
+            b >>= 1;
+        }
+        return res;
+    }
+}
