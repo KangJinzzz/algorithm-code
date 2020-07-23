5dc23fc018c96bb435f96977648a87b06639d121
kangJinzzz <1243363939@qq.com>
2020-07-23 08:16:31 +0800

    提交

--- JianZhiOffer/src/Test.java
+++ JianZhiOffer/src/Test.java
@@ -2514,3 +2514,38 @@ class Solution124 {
         return cur;
     }
 }
+
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode head = new ListNode(-1);
+        ListNode tail = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tail.next = new ListNode(l1.val);
+                l1 = l1.next;
+            } else {
+                tail.next = new ListNode(l2.val);
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+        while (l1 != null) {
+            tail.next = new ListNode(l1.val);
+            l1 = l1.next;
+            tail = tail.next;
+        }
+        while (l2 != null) {
+            tail.next = new ListNode(l2.val);
+            l2 = l2.next;
+            tail = tail.next;
+        }
+        return head.next;
+    }
+}
