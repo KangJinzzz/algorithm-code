e2b39f6415b6574cb607036f5bb2729a85bc2d23
kangJinzzz <1243363939@qq.com>
2020-08-19 08:17:36 +0800

    提交

--- JianZhiOffer/src/Test.java
+++ JianZhiOffer/src/Test.java
@@ -3555,3 +3555,24 @@ class Solution156_2 {
         return res;
     }
 }
+
+//剑指 Offer 57. 和为s的两个数字
+class Solution157 {
+    public int[] twoSum(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int[] res = new int[2];
+        while (left < right) {
+            if (nums[left] + nums[right] > target) {
+                right--;
+            } else if (nums[left] + nums[right] < target) {
+                left++;
+            } else {
+                res[0] = nums[left];
+                res[1] = nums[right];
+                break;
+            }
+        }
+        return res;
+    }
+}
