90bbc5c6bea5bf4333a061c8a4bb6ebd641b85b1
kangJinzzz <1243363939@qq.com>
2020-08-31 07:38:01 +0800

    提交

--- JianZhiOffer/src/Test.java
+++ JianZhiOffer/src/Test.java
@@ -3851,3 +3851,31 @@ class Solution165 {
         return b;
     }
 }
+
+//剑指 Offer 66. 构建乘积数组
+class Solution166 {
+    public static int[] constructArr(int[] a) {
+        if (a.length == 0) {
+            return new int[0];
+        }
+        int[] b = new int[a.length];
+        b[0] = 1;
+        for (int i = 1; i < b.length; i++) {
+            b[i] = b[i - 1] * a[i - 1];
+        }
+        int tmp = 1;
+        for (int i = b.length - 2; i >= 0; i--) {
+            tmp *= a[i + 1];
+            b[i] *= tmp;
+        }
+        return b;
+    }
+    
+    public static void main(String[] args) {
+        int[] a = new int[] {1,2,3,4,5};
+        int[] b = constructArr(a);
+        for (int x : b) {
+            System.out.print(x + " ");
+        }
+    }
+}
