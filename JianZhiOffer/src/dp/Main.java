d9fa6cc3d774ce220f5b541991b8075d4662bfd4
kangJinzzz <1243363939@qq.com>
2020-09-02 08:37:59 +0800

    提交

---
+++ JianZhiOffer/src/dp/Main.java
@@ -0,0 +1,26 @@
+package dp;
+
+public class Main {
+
+
+}
+
+public class Solution1 {
+    public int Fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        for (int i = 3; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+}
