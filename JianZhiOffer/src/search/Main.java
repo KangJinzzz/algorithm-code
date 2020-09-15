8d25b999be192e3127b5e19d886b8313a49a1d98
kangJinzzz <1243363939@qq.com>
2020-09-15 08:36:15 +0800

    提交

--- JianZhiOffer/src/search/Main.java
+++ JianZhiOffer/src/search/Main.java
@@ -24,3 +24,26 @@ class Solution1 {
         return sum;
     }
 }
+
+//733. 图像渲染
+class Solution2 {
+    int[][] position = new int[][] {{1, 0}, {-1, 0}, {0, 1}, {0, -1}} ;
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int oldColor = image[sr][sc];
+        dfs(image, sr, sc, newColor, oldColor);
+        return image;
+    }
+
+    public void dfs(int[][] image, int x, int y, int newColor, int oldColor) {
+        if (x <0 || x >= image.length || y < 0 || y >= image[0].length) {
+            return;
+        }
+        if (image[x][y] !=oldColor  || image[x][y] == newColor) {
+            return;
+        }
+        image[x][y] = newColor;
+        for (int i = 0; i < 4; i++) {
+            dfs(image, x + position[i][0], y + position[i][1], newColor, oldColor);
+        }
+    }
+}
