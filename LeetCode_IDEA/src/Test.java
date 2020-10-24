76cf6062e75546f6a7ab30e095a534ac0494dbab
kangJinzzz <1243363939@qq.com>
2020-10-24 16:44:51 +0800

    提交

--- LeetCode_IDEA/src/Test.java
+++ LeetCode_IDEA/src/Test.java
@@ -3627,3 +3627,77 @@ class soution10_22 {
         return right - left + 1;
     }
 }
+
+//    622.设计你的循环队列实现。 循环队列是一种线性数据结构，其操作表现基于 FIFO（先进先出）
+//        原则并且队尾被连接在队首之后以形成一个循环。它也被称为“环形缓冲器”。
+//        循环队列的一个好处是我们可以利用这个队列之前用过的空间。在一个普通队列里，
+//        一旦一个队列满了，我们就不能插入下一个元素，即使在队列前面仍有空间。但是使用循环队列，我们能使用这些空间去存储新的值。
+
+class MyCircularQueue_2 {
+    private int[] arr;
+    private int head = 0;
+    private int tail = 0;
+    private int size = 0;
+    /** Initialize your data structure here. Set the size of the queue to be k. */
+    public MyCircularQueue_2(int k) {
+        arr = new int[k];
+    }
+
+    /** Insert an element into the circular queue. Return true if the operation is successful. */
+    public boolean enQueue(int value) {
+        if(size == arr.length) {
+            return false;
+        }
+        arr[tail] = value;
+        size++;
+        if(tail == arr.length - 1) {
+            tail = 0;
+        } else {
+            tail++;
+        }
+        return true;
+    }
+
+    /** Delete an element from the circular queue. Return true if the operation is successful. */
+    public boolean deQueue() {
+        if(size == 0) {
+            return false;
+        }
+        if(head == arr.length - 1) {
+            head = 0;
+        } else {
+            head++;
+        }
+        size--;
+        return true;
+    }
+
+    /** Get the front item from the queue. */
+    public int Front() {
+        if(size == 0) {
+            return -1;
+        }
+        return arr[head];
+    }
+
+    /** Get the last item from the queue. */
+    public int Rear() {
+        if(size == 0) {
+            return -1;
+        }
+        if(tail == 0){
+            return arr[arr.length - 1];
+        }
+        return arr[tail - 1];
+    }
+
+    /** Checks whether the circular queue is empty or not. */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /** Checks whether the circular queue is full or not. */
+    public boolean isFull() {
+        return size == arr.length;
+    }
+}
