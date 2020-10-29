d7e3355e73ca858cbc446069db24393479d5ed3f
kangJinzzz <1243363939@qq.com>
2020-10-29 09:48:02 +0800

    提交

--- projects/dormitory/src/main/java/kang/controller/StudentController.java
+++ projects/dormitory/src/main/java/kang/controller/StudentController.java
@@ -4,7 +4,11 @@ import kang.model.Student;
 import kang.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class StudentController {
@@ -28,11 +32,47 @@ public class StudentController {
         return "index";
     }
 
+//    @RequestMapping("/find")
+//    public String add(String name, String id) {
+//        if (name == null && id == null) {
+//            return "find";
+//        }
+//        return "index";
+//    }
+
     @RequestMapping("/find")
-    public String add(String name, String id) {
-        if (name == null && id == null) {
-            return "find";
+    public String find1(Model model) {
+        List<Student> students = studentService.selectAll();
+        model.addAttribute("studentList", students);
+        return "find";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String delete(@PathVariable("id") int id) {
+        studentService.deleteByPrimaryKey(id);
+        return "redirect:/find";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String update(@PathVariable("id") int id, Model model) {
+        Student student = studentService.selectByPrimaryKey(id);
+        model.addAttribute("student", student);
+        return "update";
+    }
+
+    @RequestMapping("/update/{id}/submit")
+    public String updateSubmit(@PathVariable("id") int id, String name, String age, String sex, String address, String dormId) {
+        if (name == null || age == null || sex == null || address == null || dormId == null) {
+            return "redirect:/update/id";
         }
-        return "index";
+        Student student = new Student();
+        student.setId(id);
+        student.setName(name);
+        student.setAge(Integer.valueOf(age));
+        student.setSex(sex);
+        student.setAddress(address);
+        student.setDormId(Integer.valueOf(dormId));
+        studentService.updateByPrimaryKey(student);
+        return "redirect:/find";
     }
 }
--- projects/dormitory/src/main/java/kang/service/StudentService.java
+++ projects/dormitory/src/main/java/kang/service/StudentService.java
@@ -5,6 +5,8 @@ import kang.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class StudentService {
 
@@ -13,5 +15,21 @@ public class StudentService {
 
     public int insert(Student student) {
         return studentMapper.insert(student);
+    }
+
+    public List<Student> selectAll() {
+        return studentMapper.selectAll();
+    }
+
+    public int deleteByPrimaryKey(int id) {
+        return studentMapper.deleteByPrimaryKey(id);
+    }
+
+    public Student selectByPrimaryKey(int id) {
+        return studentMapper.selectByPrimaryKey(id);
+    }
+
+    public int updateByPrimaryKey(Student student) {
+        return studentMapper.updateByPrimaryKey(student);
     }
 }
